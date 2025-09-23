f519ab509b42a968d1f82503d7afe4758d74238a
Domitille Chebat <domitille.chebat@gmail.com>
2025-09-23 16:16:50 +0200

    package level docstring

--- src/alaeboss/__init__.py
+++ src/alaeboss/__init__.py
@@ -1,1 +1,3 @@
+"""Code to obtain imaging systematics weights for the galaxies of a spectroscopic survey catalog. The code implements linear regression on the histograms of the systematics maps, reproducing the "linear" method of recent DESI and eBOSS catalogs."""
+
 from .linear_regression import LinearRegressor
