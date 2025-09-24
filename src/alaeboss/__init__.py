8ae8eabf4001e2cd1dc0a300d838ad7cdde5b8c0
Domitille Chebat <domitille.chebat@gmail.com>
2025-09-24 14:50:32 +0200

    make produce_imweights available at the package level

--- src/alaeboss/__init__.py
+++ src/alaeboss/__init__.py
@@ -1,3 +1,4 @@
 """Code to obtain imaging systematics weights for the galaxies of a spectroscopic survey catalog. The code implements linear regression on the histograms of the systematics maps, reproducing the "linear" method of recent DESI and eBOSS catalogs."""
 
 from .linear_regression import LinearRegressor
+from .produce_imweights import produce_imweights
